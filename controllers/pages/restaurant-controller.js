313f4ec63df08ce392c0a0dbdbcfeb8aff7c8359
Elio <eliochen34@gmail.com>
2022-12-07 15:10:42 +0800

    feat: add restaurant-services

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,46 +1,9 @@
 const { Restaurant, Category, Comment, User } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantService = require('../../services/restaurant-services')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || '' // 從網址上拿下來的參數是字串轉成Number
-
-    const page = Number(req.query.page) || 1 // 來自views的內容或者第1頁
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 來自views的內容或者9筆資料
-    const offset = getOffset(limit, page) // 藉由page和limit算出offset
-
-    return Promise.all([
-      Restaurant.findAndCountAll({ // 改成findAndCountAll是為了最後要取得restaurants.count
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {} // 檢查categoryId是否為空值
-        },
-        limit, // 對restaurants DB查詢
-        offset, // 對restaurants DB查詢 => offset幾筆後，limit顯示幾筆
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
-        // console.log(req.query) => 從?page=2&categoryId=2獲得{page:2, categoryId:2}
-        const data = restaurants.rows.map(r => ({
-          ...r, // 展開運算子將每一筆restaurant的資料展開後用data接住，展開後的description會被下方新的description賦值
-          description: r.description.substring(0, 50), // 將餐廳文字描述截斷為50字元的長度
-          isFavorited: favoritedRestaurantsId.includes(r.id), // 新增一個屬性，在陣列比對過後最後回傳一個布林值，代表是是不是有被收藏
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantService.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-// const { authenticated } = require('../../middleware/auth')
 const restController = require('../../controllers/apis/restaurant-controller')
+
 router.get('/restaurants', restController.getRestaurants)
+
 module.exports = router
